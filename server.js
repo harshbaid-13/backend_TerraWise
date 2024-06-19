25ac706137e1f9f724f96e1bd9e1ccbdca162067
harshbaid-13 <harshbaid.ind@gmail.com>
2024-06-20 01:44:32 +0530

    back mode

--- server.js
+++ server.js
@@ -14,24 +14,27 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, OPTIONS, GET");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, OPTIONS, GET");
+//   next();
+// });
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(express.json(), cors(corsOptions));
-app.use("/api/users", userRoutes), cors(corsOptions);
-app.use("/api/geojson", geojsonRoutes, cors(corsOptions));
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+// app.use(express.json(), cors(corsOptions));
+// app.use("/api/users", userRoutes), cors(corsOptions);
+// app.use("/api/geojson", geojsonRoutes, cors(corsOptions));
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/geojson", geojsonRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the API server!");
